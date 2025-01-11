513586339d8e14f3bd32a75bafb24a59df5d1ed3
jom1games <jonas@saslona.com>
2025-01-11 23:13:12 +0100

    trigger DataBasePool

--- src/main/java/me/gaminglounge/gamingteams/Gamingteams.java
+++ src/main/java/me/gaminglounge/gamingteams/Gamingteams.java
@@ -1,5 +1,7 @@
 package me.gaminglounge.gamingteams;
  
+import java.sql.SQLException;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager; 
 import org.bukkit.plugin.java.JavaPlugin; 
@@ -9,11 +11,22 @@ public final class Gamingteams extends JavaPlugin {
     public static Gamingteams INSTANCE; 
     public static FileConfiguration CONFIG;
     public static TeamManager manager;
+    public DataBasePool basePool;
  
     @Override
     public void onLoad() {
         INSTANCE = this; 
         CONFIG = this.getConfig();
+
+                basePool = new DataBasePool();
+        basePool.init();
+
+        try {
+            basePool.createTablePlayer();
+            basePool.createTableTeams();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         manager = new TeamManager();
     }
