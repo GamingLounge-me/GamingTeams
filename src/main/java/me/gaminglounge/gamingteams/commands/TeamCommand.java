b2042ce625c26f34b3fda7bbe86ceb12874bef11
jom1games <jonas@saslona.com>
2025-01-11 23:06:23 +0100

    fix/add aliases

--- src/main/java/me/gaminglounge/gamingteams/commands/TeamCommand.java
+++ src/main/java/me/gaminglounge/gamingteams/commands/TeamCommand.java
@@ -1,15 +1,17 @@
 package me.gaminglounge.gamingteams.commands;
 
+import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
 import me.gaminglounge.gamingteams.Gamingteams;
 
 public class TeamCommand {
 
     public TeamCommand() {
+        Gamingteams.CONFIG.getStringList("Commands.Team.aliases").forEach(action -> CommandAPI.unregister(action));
 
         new CommandAPICommand("GamingTeams:team")
             .withPermission(Gamingteams.CONFIG.getString("Commands.Team.permission"))
-            .withAliases(Gamingteams.CONFIG.getStringList("BroadcastCommand.Aliases").toArray(num -> new String[num]))
+            .withAliases(Gamingteams.CONFIG.getStringList("Commands.Team.aliases").toArray(num -> new String[num]))
         .register();
 
     }
