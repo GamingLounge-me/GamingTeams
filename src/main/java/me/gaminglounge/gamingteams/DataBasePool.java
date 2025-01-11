7b62f1935fb0f4da9b36e4bb09348906973111aa
jom1games <jonas@saslona.com>
2025-01-11 21:52:11 +0100

    add databasepool

---
+++ src/main/java/me/gaminglounge/gamingteams/DataBasePool.java
@@ -0,0 +1,65 @@
+package me.gaminglounge.gamingteams;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DataBasePool {
+    HikariDataSource hikari;
+
+    public void init() {
+        HikariConfig config = new HikariConfig();
+        config.setPoolName("db-hikari");
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl(Gamingteams.CONFIG.getString("Database"));
+        config.setUsername(Gamingteams.CONFIG.getString("Username"));
+        config.setPassword(Gamingteams.CONFIG.getString("Password"));
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setMaxLifetime(0);
+        config.setKeepaliveTime(120000);
+        config.setConnectionTimeout(30000);
+
+        this.hikari = new HikariDataSource(config);
+        deregisterDriver("org.mariadb.jdbc.Driver");
+    }
+
+    public void shutdown() {
+        if (this.hikari != null) {
+            this.hikari.close();
+        }
+    }
+
+    private static void deregisterDriver(String driverClassName) {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            if (driver.getClass().getName().equals(driverClassName)) {
+                try {
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (this.hikari == null) {
+            throw new SQLException("Unable to get a connection from the pool. (hikari is null)");
+        }
+
+        Connection connection = this.hikari.getConnection();
+        if (connection == null) {
+            throw new SQLException("Unable to get a connection from the pool. (getConnection returned null)");
+        }
+
+        return connection;
+    }
+
+}
