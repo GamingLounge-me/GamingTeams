36fde85554a5c37b41cd4a1c6f1208a763588900
Jan Tom Förster <jan_tom@f-rster.de>
2025-01-31 19:03:13 +0100

    Added a Listener, Jon1Games is gona build the rest.

---
+++ src/main/java/me/gaminglounge/teamslistener/TeamsLeftPlayer.java
@@ -0,0 +1,35 @@
+package me.gaminglounge.teamslistener;
+
+import java.util.UUID;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class TeamsLeftPlayer extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    private final Integer teamID;
+    private final UUID member;
+
+    public TeamsLeftPlayer(Integer teamID, UUID member) {
+        this.teamID = teamID;
+        this.member = member;
+    }
+
+    public Integer getTeamID() {
+        return teamID;
+    }
+
+    public UUID removedMember() {
+        return member;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
